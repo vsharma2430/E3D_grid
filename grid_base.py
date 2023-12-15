622de124e88588b28a9041183e46707f1697e3c6
Vaibhav Sharma <59431005+vsharma2430@users.noreply.github.com>
2023-12-15 15:24:05 +0530

    UI complete - save file button remaining

---
+++ grid_base.py
@@ -0,0 +1,194 @@
+import os
+
+class point:
+    x:float
+    y:float
+    z:float
+
+    def __init__(self,x:float=0,y:float=0,z:float=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getString(self)-> str:
+        return str(self.x) + ',' + str(self.y) + ',' +str(self.z)
+    
+    def getPMLString(self)-> str:
+        return 'E '+str(self.x) + 'mm N ' + str(self.y) + ' mm U ' +str(self.z) + 'mm'
+    
+class grid_data:
+    grid_label:str
+    grid_delta:point
+
+    def __init__(self,label:str='LBL',delta:point=point(0,0,0)):
+        self.grid_label = label
+        self.grid_delta = delta
+    
+    def getString(self):
+        return self.grid_label + ' ' + self.grid_delta.getString()
+
+def add_str(*argv)-> str:
+    res:str=''
+    for arg in argv:
+        res += str(arg)
+    return res
+
+def add_list(list_str:[str])-> str:
+    res:str=''
+    for dataX in list_str:
+        res += str(dataX) + '\n'
+    return res
+
+def enclose_quotes(data:str)-> str:
+    return '\''+data+'\''
+
+def enclose_notation(data:str)-> str:
+    return '@'+data+'@'
+
+def add_data(*argv,**kwargs) -> str:
+    res_str : str = ''
+    
+    for key, value in kwargs.items():
+        key = str(key)
+        value = str(value)
+        if(value.startswith('@') and value.endswith('@')):
+            res_str = res_str + key + ' ' + enclose_quotes(value.strip('@')) + '\n'
+        elif(value=='' or key==''):
+            continue
+        else:
+            res_str = res_str + key + ' ' + value + '\n'
+    res_str+='\n'
+    res_str += add_str(*argv)
+    return res_str
+
+def enclose_start_end_function(enclosure:str='')-> str:
+    return lambda data='',name='': 'NEW '+ enclosure + ' '+ name + (('\n' + data) if len(str(data))>0 else '') + '\nEND' 
+
+grid_world_enclose = enclose_start_end_function('GRIDWLD')
+idinfo_enclose = enclose_start_end_function('IDINFO')
+facelb_enclose = enclose_start_end_function('FACELB')
+elevlb_enclose = enclose_start_end_function('ELEVLB')
+refgrd_enclose = enclose_start_end_function('REFGRD')
+gridfaces_enclose = enclose_start_end_function('GRIDFACES')
+gridpl_enclose = enclose_start_end_function('GRIDPL')
+gridelevation_enclose = enclose_start_end_function('GRIDELEVATION')
+refgln_enclose = enclose_start_end_function('REFGLN')
+
+def form_facelb(grllbl:str,starti:str,gridaxe:str)-> str:
+    return facelb_enclose(data=add_data(**{'GRDLBL':enclose_notation(grllbl),'STARTI':enclose_notation(starti),'STEPID':'1','GRDAXE':enclose_notation(gridaxe)}))
+
+def form_elevlb(grllbl:str,starti:str,gridaxe:str)-> str:
+    return elevlb_enclose(data=add_data(**{'GRDLBL':enclose_notation(grllbl),'STARTI':enclose_notation(starti),'STEPID':'1','GRDAXE':enclose_notation(gridaxe)}))
+
+def form_refgrd(ref_name:str,origin:point,gridfaces:str)-> str:
+    res:str = ''
+    res += add_data(**{'POS':origin.getPMLString()})
+    res += gridfaces
+    return refgrd_enclose(data=res,name=ref_name)
+
+def form_gridface(grllbl:str,starti:str,gridaxe:str,grid_planes:str)-> str:
+    return gridfaces_enclose(data=add_data(grid_planes,**{'GRDLBL':enclose_notation(grllbl),'STARTI':enclose_notation(starti),'STEPID':'1','GRDAXE':enclose_notation(gridaxe)}))
+
+def form_gridelevation(grllbl:str,starti:str,gridaxe:str,grid_planes:str)-> str:
+    return gridelevation_enclose(data=add_data(grid_planes,**{'GRDLBL':enclose_notation(grllbl),'STARTI':enclose_notation(starti),'STEPID':'1','GRDAXE':enclose_notation(gridaxe)}))
+
+def form_gridplanes(posn:point,ori:str,idplan:str,no_refgln:int)-> str:
+    refgln:str=''
+    for i in range(no_refgln):
+        refgln += refgln_enclose() + '\n'
+    return gridpl_enclose(data=add_data(refgln,**{'POS':posn.getPMLString(),'ORI':ori,'IDPLAN':enclose_notation(idplan)}))
+
+
+def relation_grid(gridFace1:int,gridFace2:int,ref_grid:str,ref_gln_no:str,grid_faces1_no:str,grid_pl1_no:str,grid_faces2_no:str,grid_pl2_no:str)->str:
+    res:str=''
+    if(gridFace1==1):
+        res += f'OLD REFGLN {ref_gln_no} of GRIDPL {grid_pl1_no} of GRIDFACES {grid_faces1_no} of REFGRD {ref_grid}\n'
+    else:
+        res += f'OLD REFGLN {ref_gln_no} of GRIDPL {grid_pl1_no} of GRIDELEVATION {grid_faces1_no} of REFGRD {ref_grid}\n'
+    if(gridFace2==1):
+        res += f'GRDREF GRIDPL {grid_pl2_no} of GRIDFACES {grid_faces2_no} of REFGRD {ref_grid}\n'
+    else:
+        res += f'GRDREF GRIDPL {grid_pl2_no} of GRIDELEVATION {grid_faces2_no} of REFGRD {ref_grid}\n'
+    return res
+
+#ref_grid = grid_data('/TEST_GRID',point(3687250,3640000,99700))
+#x_grid = [grid_data('1/B11',point(0,0,0)),grid_data('2/B11',point(19500,0,0)),grid_data('3/B11',point(26500,0,0)),grid_data('4/B11',point(32750,0,0)),grid_data('5/B11',point(40500,0,0)),grid_data('6/B11',point(48750,0,0))]
+#y_grid = [grid_data('X7',point(0,0,0)),grid_data('X8',point(0,10000,0)),grid_data('X9',point(0,15000,0))]
+#z_grid = [grid_data('1',point(0,0,0))]
+#new_old : bool = True
+#out_file_location:str=''
+#os.getcwd()+r'\E3D_grid\Result\Grid_Macro.mac'
+
+# FINAL DB FORMATION
+def build_macro(ref_grid,x_grid,y_grid,z_grid,new_old,out_file_location):
+    pml_db:str=''
+    #pml_db = add_str(pml_db,'INPUT BEGIN' + '\n')
+
+    if(new_old):
+        pml_db = add_str(pml_db,form_facelb(grllbl='Axis',starti='1',gridaxe='X')+ '\n')
+        pml_db = add_str(pml_db,form_facelb(grllbl='Row' ,starti='A',gridaxe='Y')+ '\n')
+        pml_db = add_str(pml_db,form_elevlb(grllbl='Elev',starti='1',gridaxe='Z')+ '\n')
+        pml_db = idinfo_enclose(pml_db)
+
+    x_grid_planes:str=''
+    y_grid_planes:str=''
+    z_grid_planes:str=''
+    for xgrd in x_grid:
+        x_grid_planes = add_str(x_grid_planes,form_gridplanes(xgrd.grid_delta,'Y is U and Z is W',xgrd.grid_label,len(y_grid)+len(z_grid))) + '\n'
+    for ygrd in y_grid:
+        y_grid_planes = add_str(y_grid_planes,form_gridplanes(ygrd.grid_delta,'Y is U and Z is S',ygrd.grid_label,len(x_grid)+len(z_grid))) + '\n'
+    for zgrd in z_grid:
+        z_grid_planes = add_str(z_grid_planes,form_gridplanes(zgrd.grid_delta,'',zgrd.grid_label,len(x_grid)+len(y_grid))) + '\n'
+
+    grid_faces=list()
+    grid_faces.append(form_gridface(grllbl='Axis',starti='1',gridaxe='X',grid_planes=x_grid_planes))
+    grid_faces.append(form_gridface(grllbl='Row',starti='A',gridaxe='Y',grid_planes=y_grid_planes))
+    grid_faces.append(form_gridelevation(grllbl='Elev',starti='1',gridaxe='Z',grid_planes=z_grid_planes))
+
+    grid_faces_final = add_list(grid_faces)
+    ref_grid_final = form_refgrd(ref_name=ref_grid.grid_label,origin=ref_grid.grid_delta,gridfaces=grid_faces_final)
+
+    if(new_old):
+        pml_db = grid_world_enclose(pml_db + '\n' + ref_grid_final)
+
+    #pml_db = add_str(pml_db,'INPUT FINISH' + '\n')
+
+    print('Opening file ' + out_file_location)
+    f = open(out_file_location, "w")
+    f.write(pml_db)
+    f.close()
+    print('File closed')
+
+
+#GRID RELATIONS CODE - NOT REQUIRED
+"""
+for i in range(len(x_grid)):
+    for j in range(len(y_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=1,gridFace2=1,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=j+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=j+1,grid_faces2_no=2))
+    for k in range(len(z_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=1,gridFace2=2,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=len(y_grid)+k+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=k+1,grid_faces2_no=1))
+        
+for i in range(len(y_grid)):
+    for j in range(len(x_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=1,gridFace2=1,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=j+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=j+1,grid_faces2_no=2))
+    for k in range(len(z_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=1,gridFace2=2,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=len(x_grid)+k+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=k+1,grid_faces2_no=1))
+        
+for i in range(len(z_grid)):
+    for j in range(len(x_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=2,gridFace2=1,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=j+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=j+1,grid_faces2_no=2))
+    for k in range(len(y_grid)):
+        pml_db = add_str(pml_db,'\n'+\
+                relation_grid(gridFace1=2,gridFace2=1,ref_grid=ref_grid.grid_label,
+                              ref_gln_no=len(x_grid)+k+1,grid_pl1_no=i+1,grid_faces1_no=1,grid_pl2_no=k+1,grid_faces2_no=1))
+"""
---
+++ grid_ui.py
@@ -0,0 +1,215 @@
+import tkinter as tk 
+from tkinter import messagebox
+import os
+from os.path import basename
+from grid_base import grid_data,point
+from grid_base import build_macro
+
+def selectFile ():
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+    Tk().withdraw()
+    filename = askopenfilename()
+    return (filename)
+
+def saveFile():
+    from tkinter.filedialog import asksaveasfile 
+    root = Tk() 
+    root.geometry('200x150') 
+    def save(): 
+        files = [('All Files', '*.*'),  
+                ('Macro Files', '*.mac')] 
+        file = asksaveasfile(filetypes = files, defaultextension = files) 
+    
+    btn = ttk.Button(root, text = 'Save', command = lambda : save()) 
+    btn.pack(side = TOP, pady = 20) 
+
+def readLineByLine (fileLocation) : 
+    file1 = open(fileLocation, 'r')
+    Lines = file1.readlines()
+    readLines = []
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        readLines.append(line.strip())
+        #print("Line{}: {}".format(count, line.strip()))
+    return readLines
+
+def getGridList(grid_names:str,grid_values:str,dir:int):
+    grid_list = []
+    grid_val_split = grid_values.split(' ')
+    grid_name_split = grid_names.split(' ')
+    grid_vals = []
+    for valX in grid_val_split:
+        if(str(valX).find('*')>0):
+            grid_valX_split = str(valX).split('*')
+            rep = int(grid_valX_split[0].strip())
+            valX = float(grid_valX_split[1].strip())
+            for i in range(rep):
+                grid_vals.append(float(valX))
+        else:
+            grid_vals.append(valX)
+
+    if(len(grid_name_split)==len(grid_vals)):
+        sum :float= 0
+        for i in range(len(grid_vals)):
+            grid_delta:point=point(0,0,0)
+            if(dir==1):
+                grid_delta = point(sum+float(grid_vals[i]),0,0)
+            elif(dir==2):
+                grid_delta = point(0,sum+float(grid_vals[i]),0)
+            else:
+                grid_delta = point(0,0,sum+float(grid_vals[i]))
+            grid_data_X = grid_data(label=grid_name_split[i],delta=grid_delta)
+            sum += float(grid_vals[i])
+            print(grid_data_X.getString())
+            grid_list.append(grid_data_X)
+
+    return grid_list
+
+saveLoc = os.getenv('LOCALAPPDATA') + r"\GridE3D"
+saveFile = saveLoc + r"\db.dat"
+
+if(not os.path.isdir(saveLoc)):
+    os.mkdir(saveLoc)
+
+
+# Top level window 
+frame = tk.Tk() 
+frame.title("Grid E3D 1.0") 
+frame.geometry('800x400') 
+
+def selectOutputFile():
+    output_file = selectFile()
+    input_file.delete(1.0,"end")
+    input_file.insert(tk.END,output_file)
+
+label_width = 10
+input_width = 80
+
+framereference = tk.Frame(frame)
+label_reference = tk.Label(framereference, text = "Reference ",width=label_width,justify="left",anchor="w") 
+label_reference.pack(side="left")
+framereferenceInput=tk.Frame(framereference)
+input_reference_name = tk.Text(framereferenceInput, height = 1, width=int(input_width*0.25))
+input_reference_X = tk.Text(framereferenceInput, height = 1, width=int(input_width*0.25))
+input_reference_Y = tk.Text(framereferenceInput, height = 1, width=int(input_width*0.25))
+input_reference_Z = tk.Text(framereferenceInput, height = 1, width=int(input_width*0.25))
+input_reference_name.pack(side="left")
+input_reference_X.pack(side="left")
+input_reference_Y.pack(side="left")
+input_reference_Z.pack(side="left")
+framereferenceInput.pack(side="left")
+framereference.pack(padx=5,pady=5)
+
+frameGridX = tk.Frame(frame)
+label_gridX = tk.Label(frameGridX, text = "X Axis",width=label_width,justify="left",anchor="w") 
+label_gridX.pack(side="left")
+frameGridXInput=tk.Frame(frameGridX)
+input_gridX_name = tk.Text(frameGridXInput, height = 1, width=input_width)
+input_gridX_value = tk.Text(frameGridXInput, height = 1, width=input_width)
+input_gridX_value.pack(side="top")
+input_gridX_name.pack(side="top")
+frameGridXInput.pack(side="left")
+frameGridX.pack(padx=5,pady=5)
+
+frameGridY = tk.Frame(frame)
+label_gridY = tk.Label(frameGridY, text = "Y Axis",width=label_width,justify="left",anchor="w") 
+label_gridY.pack(side="left")
+frameGridYInput=tk.Frame(frameGridY)
+input_gridY_name = tk.Text(frameGridYInput, height = 1, width=input_width)
+input_gridY_value = tk.Text(frameGridYInput, height = 1, width=input_width)
+input_gridY_value.pack(side="top")
+input_gridY_name.pack(side="top")
+frameGridYInput.pack(side="left")
+frameGridY.pack(padx=5,pady=5)
+
+frameGridZ = tk.Frame(frame)
+label_gridZ = tk.Label(frameGridZ, text = "Z Axis",width=label_width,justify="left",anchor="w") 
+label_gridZ.pack(side="left")
+frameGridZInput=tk.Frame(frameGridZ)
+input_gridZ_name = tk.Text(frameGridZInput, height = 1, width=input_width)
+input_gridZ_value = tk.Text(frameGridZInput, height = 1, width=input_width)
+input_gridZ_value.pack(side="top")
+input_gridZ_name.pack(side="top")
+frameGridZInput.pack(side="left")
+frameGridZ.pack(padx=5,pady=5)
+
+new_old:bool = True
+def sel():
+    if(var.get()==1):
+        new_old = True
+    else:
+        new_old = False
+   #selection = "You selected the option " + str(var.get())
+   #print(selection)
+
+var = tk.IntVar()
+frameGridType = tk.Frame(frame)
+label_GridType = tk.Label(frameGridType, text = "Grid Type",width=label_width,justify="left",anchor="w") 
+label_GridType.pack(side="left")
+frameGridTypeInput=tk.Frame(frameGridType)
+input_GridType_New = tk.Radiobutton(frameGridTypeInput, text="New", variable=var, value=1, command=sel,width=int(input_width*1.1),anchor=tk.W)
+input_GridType_New.select()
+input_GridType_Old = tk.Radiobutton(frameGridTypeInput, text="Old", variable=var, value=2, command=sel,width=int(input_width*1.1),anchor=tk.W)
+input_GridType_New.pack(side="top",anchor=tk.W)
+input_GridType_Old.pack(side="top",anchor=tk.W)
+frameGridTypeInput.pack(side="left",anchor=tk.W)
+frameGridType.pack(padx=5,pady=5)
+
+
+frameOutput = tk.Frame(frame)
+labelFile = tk.Label(frameOutput, text = "Output",width=label_width,justify="left",anchor="w")  
+input_file = tk.Text(frameOutput, height = 1, width = int(input_width*0.8)) 
+input_button = tk.Button(frameOutput, text = "Select", command = saveFile,width=int(input_width*0.2),height=1) 
+labelFile.pack(side="left")
+input_file.pack(side="left")
+input_button.pack(side="left")
+frameOutput.pack(padx=5,pady=5)
+
+frameEIL = tk.Frame(frame)
+lblName1 = tk.Label(frameEIL, text = "Developed with ❤ by Vaibhav Sharma",font=("Comic Sans MS",10) ,anchor="w",width=50)  
+lblName2 = tk.Label(frameEIL, text = "©Engineers India Limited",font=("Comic Sans MS", 10) ,anchor="e",width=50) 
+lblName1.pack(side="left")
+lblName2.pack(side="right")
+
+input_reference_name.insert(1.0,'MYGRID')
+input_reference_X.insert(1.0,'1000')
+input_reference_Y.insert(1.0,'2000')
+input_reference_Z.insert(1.0,'3000')
+input_gridX_name.insert(1.0,'A B C D')
+input_gridX_value.insert(1.0,'0 1000 2*2000')
+input_gridY_name.insert(1.0,'1 2 3')
+input_gridY_value.insert(1.0,'0 2*2500')
+input_gridZ_name.insert(1.0,'1')
+input_gridZ_value.insert(1.0,'0')
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+input_file.insert(1.0,desktop+r'\macro.mac')
+
+def printInput(): 
+    gridXName = input_gridX_name.get(1.0, "end-1c") 
+    gridYName = input_gridY_name.get(1.0, "end-1c") 
+    gridZName = input_gridZ_name.get(1.0, "end-1c") 
+    gridXVal = input_gridX_value.get(1.0, "end-1c") 
+    gridYVal = input_gridY_value.get(1.0, "end-1c") 
+    gridZVal = input_gridZ_value.get(1.0, "end-1c") 
+    grid_refX = float(input_reference_X.get(1.0, "end-1c"))
+    grid_refY = float(input_reference_Y.get(1.0, "end-1c"))
+    grid_refZ = float(input_reference_Z.get(1.0, "end-1c"))
+    grid_ref_name = 'LBL'
+    out_file_location = input_file.get(1.0, "end-1c") 
+    grid_ref = grid_data(label='/'+grid_ref_name,delta=point(grid_refX,grid_refY,grid_refZ))
+    x_grid = getGridList(grid_names=gridXName,grid_values=gridXVal,dir=1)
+    y_grid = getGridList(grid_names=gridYName,grid_values=gridYVal,dir=2)
+    z_grid = getGridList(grid_names=gridZName,grid_values=gridZVal,dir=3)
+    build_macro(grid_ref,x_grid,y_grid,z_grid,new_old,out_file_location)
+
+    return
+
+# Button Creation 
+createButton = tk.Button(frame, text = "Create Macro",  command = printInput,width=25,height=2) 
+createButton.pack(padx=10,pady=10) 
+frameEIL.pack(padx=5,pady=5,anchor=tk.S)
+
+frame.mainloop() 
